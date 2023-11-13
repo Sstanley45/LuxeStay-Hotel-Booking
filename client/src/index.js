465947595c840d800068f91973c1834faceeda05
Sstanley45 <stanleynjuguna445@gmail.com>
2023-11-13 10:32:20 +0300

    added 'basename' to browser router

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { AppProvider } from './context';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AppProvider } from "./context";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <AppProvider> 
-    <App />
-    </AppProvider>
+    <BrowserRouter basename="/">
+      <AppProvider>
+        <App />
+      </AppProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
